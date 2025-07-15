35885a97126175eb50826b1c8473e09f4dc1013e
Persfone <persfone240@gmail.com>
2025-07-15 15:32:21 -0300

    elimine archivos inecesarios del proyecto

--- Comida.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-public class Comida : MonoBehaviour
-{
-    public float valorNutricional = 20f; // configurable en el Inspector
-}
--- bebida.cs
+++
@@ -1,6 +0,0 @@
-﻿using UnityEngine;
-
-public class Bebida : MonoBehaviour
-{
-    public float valorHidratacion = 20f; // configurable en el Inspector
-}
